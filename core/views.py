5d490c9f34e82691e0c5ea3456421e5f34afc985
Alex Mwaniki Nyambura <lekko254@gmail.com>
2023-12-30 03:38:54 +0300

    Update index.html with new item and category display

--- core/views.py
+++ core/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render
 
+from item.models import Category, Item
+
 def index(request):
-        return render(request, 'core/index.html')
+        items = Item.objects.filter(is_sold=False)[0:6]
+        categories = Category.objects.all()
+        context = {
+                'items': items,
+                'categories': categories
+        }
+        return render(request, 'core/index.html', context)
 
 def contact(request):
         return render(request, 'core/contact.html')
