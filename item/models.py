6d6ea7ea6acfe7baa5f81e589f55b89cf1103485
Alex Mwaniki Nyambura <lekko254@gmail.com>
2023-12-30 03:12:22 +0300

    Add Item model and import User model

--- item/models.py
+++ item/models.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 class Category(models.Model):
@@ -9,3 +10,14 @@ class Category(models.Model):
 
     def __str__(self):
                 return self.name
+    
+class Item(models.Model):
+        category = models.ForeignKey(Category, related_name='items', on_delete=models.CASCADE)
+        name = models.CharField(max_length=255)
+        description = models.TextField(blank=True, null=True)
+        price = models.FloatField()
+        image = models.ImageField(upload_to='item_images', blank=True, null=True)
+        is_sold = models.BooleanField(default=False)
+        created_by = models.ForeignKey(User, related_name='items', on_delete=models.CASCADE)
+        created_at = models.DateTimeField(auto_now_add=True)
+        
