048ca1fc6154c62053caae1f9ab691dc9613b79e
Alex Mwaniki Nyambura <lekko254@gmail.com>
2023-12-30 03:05:22 +0300

    Add 'item' app to INSTALLED_APPS

---
+++ item/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+
+    class Meta:
+        ordering = ('name',)
+        verbose_name_plural = 'Categories'
+
+    def __str__(self):
+                return self.name
--- store/settings.py
+++ store/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'core', # app criada
+    'item', # app criada
 ]
 
 MIDDLEWARE = [
